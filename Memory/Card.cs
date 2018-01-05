8eb1bf48980863a4ba0ee595444b9e46b5ba80f1
zysktomasz <zysk.tomasz95@gmail.com>
2018-01-05 11:12:19 +0100

    wstepnie ogarniete uzywanie .png jako Resource

---
+++ Memory/Card.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace Memory
+{
+    class Card
+    {
+        public Rectangle CorrespondingRectangle { get; set; }
+        public string FileName { get; set; }
+        public int Occurence { get; set; }
+
+        public List<Card> CardList { get; set; }
+
+
+        public Card(Rectangle correspondingRectangle, string fileName)
+        {
+            CorrespondingRectangle = correspondingRectangle;
+            FileName = fileName;
+            Occurence = 2;
+        }
+
+        public void GenerateAllCards(List<Rectangle> RectangleList)
+        {
+
+            int CardsToGenerate = RectangleList.Count / 2;
+
+            // dziala dla kazdej pary
+            for (int i = 0; i < CardsToGenerate; i++)
+            {
+                // pobiera losowe zdjecie dla pary
+                Random rnd = new Random();
+                rnd.Next(FileListExtract.FileList.Count - 1);
+
+                // pobiera losowy rectangle (dwukrotnie)
+                //rnd.Next(RectangleList.Count - 1);
+
+
+                //CardList.Add()
+            }
+            
+
+        }
+
+    }
+}
--- Memory/MainWindow.xaml.cs
+++ Memory/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,57 @@ namespace Memory
         public MainWindow()
         {
             InitializeComponent();
+
+            // utworzenie tablicy z dodanymi do projektu zdjeciami Kart
+            FileListExtract.FindAllFiles();
+
+            //List<Rectangle> RectangleList = new List<Rectangle>
+            //{
+            //    rectangle_1, rectangle_2, rectangle_3, rectangle_4, rectangle_5,rectangle_6,rectangle_7,rectangle_8,
+            //    rectangle_9,
+            //    rectangle_10,rectangle_11,rectangle_12
+            //};
+
+            // dodanie eventow do eventhandlera
+
+            rectangle_1.MouseLeftButtonDown += Rectangle_MouseLeftButtonDown;
+            rectangle_2.MouseLeftButtonDown += Rectangle_MouseLeftButtonDown;
+            rectangle_3.MouseLeftButtonDown += Rectangle_MouseLeftButtonDown;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            
+            przycisk.Content = "XDDDD";
+            
+        }
+        
+        
+        // utworzenie eventu
+        void Rectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // test czy dziala klikniecie w rectangle
+            Rectangle R = (Rectangle)sender;
+            
+
+            // DZIALA DLA Resource OBRAZOW
+            R.Fill = new ImageBrush
+            {
+                //ImageSource = new BitmapImage(new Uri(@"img\cat_1.png", UriKind.RelativeOrAbsolute))
+                //ImageSource = new BitmapImage(new Uri(
+                //                    "pack://application:,,,/img/1_fish.png"))
+
+                ImageSource = new BitmapImage(new Uri(String.Format("pack://application:,,,/{0}", FileListExtract.FileList[10])
+                                    ))
+            };
+
+            
+            
+            
+            
+
+
         }
     }
 }
